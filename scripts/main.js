06bc9e941ab9c5d657b07cb7e0771b2db7b8728b
JoeAndyC <joelcuenca.jc@gmail.com>
2025-08-17 01:58:28 -0300

    Se agregó las clases de operación a la división y al porcentaje, además de funciones para los botones en el main.js, y cambios en el css

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
 const numeros = document.querySelectorAll('.numero');
+const operaciones = document.querySelectorAll('.operacion');
+const reStart = document.querySelector('#reStart');
 const screenP = document.querySelector('#resultado');
+let valActual;
+let valAnterior;
+
 
 numeros.forEach((numero) => {
   numero.addEventListener('click', () => {
     screenP.textContent += numero.innerText;
+  });
+});
+
+reStart.addEventListener('click', () => {
+  screenP.textContent = '';
+});
+
+operaciones.forEach((operacion) => {
+  operacion.addEventListener('click', () => {
+    screenP.textContent += ` ${operacion.innerText} `;
   });
 });
